3fd67be3102e99329a4c19d0bf23d40b885b21c5
LFirmiano <lucas.firmianosg@gmail.com>
2020-09-24 11:42:40 -0300

    Cadastro de Estoque

--- include/C_estoque.php
+++ include/C_estoque.php
@@ -3,7 +3,8 @@
 include "bd.php";
 
 $i=0;
-$j=1;
+$aux=1;
+$aux2=2;
 
 $array = array();
 
@@ -12,65 +13,27 @@ foreach ($_POST as $row){
     $i++;
 }
 
+$a1 = 3;
+$r = 2;
+$n = 1 + ($i - $a1)/$r;
+
 $query = "INSERT INTO estoque (unidade,tamanho,quantidade) VALUES (:unidade,:tamanho,:quantidade)";
 $stmt = $conn->prepare($query);
 
-$aux = 1;
-$aux2 = 2;
+for ($j=1;$j<=$n;$j++){
 
-// 5 -> 2
-// 7 -> 3
-// 9 -> 4
+    $stmt->bindParam(':unidade',$array[0],PDO::PARAM_STR);
+    $stmt->bindParam(':tamanho',$array[$aux],PDO::PARAM_STR);
+    $stmt->bindParam(':quantidade',$array[$aux2],PDO::PARAM_STR);
 
-for ($j=0;$j<$i;$j++){
-
-    echo $array[$j+$aux] . "<br>";
-    echo $array[$j+$aux2];
-
-    echo "<br>";
-
-    $aux = $aux + 2;
+    $aux = $j + 2;
     $aux2 = $aux + 1;
-    // $stmt->bindParam(':unidade',$array[0],PDO::PARAM_STR);
-    // $stmt->bindParam(':tamanho',$array[$j],PDO::PARAM_STR);
-    // $stmt->bindParam(':quantidade',$array[$k],PDO::PARAM_STR);
 
-    // $stmt->execute();
+    $stmt->execute();
 
 }
 
-// header("Location: ../");
-
-// // ISSO NÃO SERÁ ASSIM
-// if($stmt->execute()){
-//     header("Location: ../cad_estoque.php");
-// } else {
-//     header("Location: ../cad_produto.php");
-// }
-
-
-// $_POST = {
-//     "id" => "Unidades Calçados",
-//     "35-36" => "60",
-//     "38-39" => "100"
-// }
-
-// foreach ($_POST as $row){
-
-// }
-
-// $array = {
-//     "0" => "Unidades Calçados",
-//     "1" => "60",
-//     "2" => "100"
-// }
-
-// $referencia = ["35-36","38-39"]
-
-// for ($i=1;$i<len($array);$i++){
-//     $j = $i-1;
-//     $sql = "INSERT INTO estoque (unidade,tamanho,valor) VALUES ($array["0"],$referencia[$j],$array[$i])"
-// }
+header("Location: ../");
 
 
 ?>
--- include/bd.php
+++ include/bd.php
@@ -3,23 +3,23 @@
 // ini_set('display_errors', true); error_reporting(E_ALL);
 
 // CONFIG CABEMCE
-// $host = "localhost"	;
-// $database = "kg";
-// $user = "postgres";
-// $password = "cabemce2014";
+$host = "localhost"	;
+$database = "kg";
+$user = "postgres";
+$password = "cabemce2014";
 
-// $dsn = "pgsql:host=$host;port=5432;dbname=$database;user=$user;password=$password";
+$dsn = "pgsql:host=$host;port=5432;dbname=$database;user=$user;password=$password";
 
 // CONFIG CASA
 
-$host = "localhost"	;
-$database = "kg";
-$user = "root";
-$password = "";
+// $host = "localhost"	;
+// $database = "kg";
+// $user = "root";
+// $password = "";
 
 //MUDAR PARA MYSQL COM A PORTA CORRETA
 
-$dsn = "mysql:host=$host;dbname=$database;user=$user;password=$password";
+// $dsn = "mysql:host=$host;dbname=$database;user=$user;password=$password";
 
 try{
     $conn = new PDO($dsn);
