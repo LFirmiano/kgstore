db0226ad5d6dccc4d14e17f6e8a35b3227bde25a
LFirmiano <lucas.firmianosg@gmail.com>
2020-09-28 19:02:32 -0300

    R cliente e fornecedor

---
+++ include/D_cliente.php
@@ -0,0 +1,12 @@
+<?php 
+
+// ini_set('display_errors', true); error_reporting(E_ALL);
+
+include_once("bd.php");
+
+echo $_POST['excluir'];
+
+// $stmt = $conn->prepare("DELETE FROM cliente WHERE");
+// $stmt->execute();
+
+?>
--- include/L_estoque.php
+++ include/L_estoque.php
@@ -7,19 +7,4 @@ include_once("bd.php");
 $stmt = $conn->prepare("SELECT * FROM produtos");
 $stmt->execute();
 
-// var_dump($stmt->fetch(PDO::FETCH_OBJ));
-
-// if($stmt->execute()){
-//     while($row = $stmt->fetch(PDO::FETCH_OBJ)){
-//         echo $row->produto;
-//         // $result[] = array(
-//         //     'tamanhos'	=> $row->tamanho,
-
-//         // );
-//     }
-// }
-// } else {
-//     echo "<br> NÃO FOI POSSIVEL SELECIONAR <br>";
-// }
-
 ?>
