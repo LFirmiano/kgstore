4fdd274c6b4354b6f535dd8ec760ef5f4265541b
Gabriielfeliciano <gabrielfelicianogo@gmail.com>
2020-09-30 15:22:43 -0300

    telas pedidos

--- cad_categoria.php
+++ cad_categoria.php
@@ -34,8 +34,21 @@
   <div class="form-group col-md-4">
     <label for="exampleFormControlInput1"><strong>SubCategoria</strong></label>
     <input type="text" class="form-control" name="subcategoria" placeholder="Ex:Short Jeans Com Pontas Rasgadas">
-  </div></div>
+  </div>
+      
+   <div class="form-group col-md-4">      
+   <label for="exampleFormControlSelect1"><strong>Unidade de Medida</strong></label>
+    <select class="form-control" onchange="getval(this)" name="id" id="id">
+    <!-- Unidadade que vai ser usada no estoque-->
+      <option value="">Selecione a Unidade</option>
+      <option value="Unidades Calçados">Unidades Calçados</option>
+      <option value="Unidades Roupas Inferiores">Unidades Roupas Inferiores</option>
+      <option value="Unidades Básicas">Unidades Básicas</option>
+      <option value="Unidade Única">Unidade Única</option>
+    </select>
+    </div>
   
+    </div>
   <a href="categoria.php" class="btn btn-outline-dark" style="margin-bottom:2%;"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
   </svg></a> 
