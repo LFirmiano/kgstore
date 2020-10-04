1a72907add40f57d06afc2a1b2f5e605f96795db
LFirmiano <lucas.firmianosg@gmail.com>
2020-10-04 11:30:17 -0300

    U PROD E ESTOQUE

---
+++ include/U_estoque.php
@@ -0,0 +1,38 @@
+<?php 
+
+include "bd.php";
+
+$i=0;
+$aux=1;
+$aux2=2;
+
+$array = array();
+
+foreach ($_POST as $row){
+    $array[$i] = $row;
+    $i++;
+}
+
+$a1 = 3;
+$r = 2;
+$n = 1 + ($i - $a1)/$r;
+
+$query2 = "UPDATE estoque SET quantidade = :quantidade WHERE produto = :produto AND tamanho = :tamanho";
+$stmt2 = $conn->prepare($query2);
+
+for ($j=1;$j<=$n;$j++){
+
+    $stmt2->bindParam(':quantidade',$array[$aux2],PDO::PARAM_STR);
+    $stmt2->bindParam(':produto',$array[0],PDO::PARAM_STR);
+    $stmt2->bindParam(':tamanho',$array[$aux],PDO::PARAM_STR);
+
+    $aux = $j + 2;
+    $aux2 = $aux + 1;
+
+    $stmt2->execute();
+
+}
+
+header("Location: ../estoque.php");
+
+?>
