17442a4f92c3052067c0e40a812e6a17a050269b
LFirmiano <lucas.firmianosg@gmail.com>
2020-10-05 15:40:34 -0300

    CORRECAO EDIT PRODUTO

--- include/U_produto.php
+++ include/U_produto.php
@@ -3,15 +3,22 @@
 include "bd.php";
 
 $query = "UPDATE produtos SET produto = :produto, categoria = :categoria, fornecedor = :fornecedor, valor = :valor WHERE id_produto = :id";
+$sql = "UPDATE estoque SET produto = :produtoest WHERE produto = :produtold";
 
 $stmt = $conn->prepare($query);
+$stmt2 = $conn->prepare($sql);
 $stmt->bindParam(':produto',$_POST['produto'],PDO::PARAM_STR);
+$stmt2->bindParam(':produtoest',$_POST['produto'],PDO::PARAM_STR);
+$stmt2->bindParam(':produtold',$_POST['produto_old'],PDO::PARAM_STR);
 $stmt->bindParam(':categoria',$_POST['categoria'],PDO::PARAM_STR);
 $stmt->bindParam(':fornecedor',$_POST['fornecedor'],PDO::PARAM_STR);
 $stmt->bindParam(':valor',$_POST['valor'],PDO::PARAM_STR);
 $stmt->bindParam(':id',$_POST['edit'],PDO::PARAM_STR);
 
+
+
 $stmt->execute();
+$stmt2->execute();
 
 header("Location: ../estoque.php");
 
