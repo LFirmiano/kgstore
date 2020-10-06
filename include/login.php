d3016e35cea5c61ffade599ecd7dd281ff389db3
LFirmiano <lucas.firmianosg@gmail.com>
2020-10-05 21:09:46 -0300

    LOGIN, LOGOUT, LOGADO

--- include/login.php
+++ include/login.php
@@ -8,18 +8,19 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 $ident = $_POST['tipo'];
 
-$query = "SELECT * FROM usuario WHERE email = :email";
+$query = "SELECT * FROM usuarios WHERE email = :email";
 $stmt = $conn->prepare($query);
 $stmt->bindParam(':email',$email,PDO::PARAM_STR);
 if ($stmt->execute()){
     $row = $stmt->fetch(PDO::FETCH_OBJ);
-    $db_email = $row['email'];
-    $db_senha = $row['senha'];
+    $db_email = $row->email;
+    $db_senha = $row->senha;
     if ($db_email == $email && $db_senha == $senha){
         $_SESSION['email'] = $db_email;
         $_SESSION['senha'] = $db_senha;
+        $_SESSION['tipo'] = $ident;
         if ($ident == 0){
-            header("Location: cliente.php");
+            header("Location: ../cliente.php");
         }
         if ($ident == 1){
             header("Location: cliente.php");
