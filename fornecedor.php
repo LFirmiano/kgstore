9b8314b79b584ad58fba733a47c6611819c8a75e
Gabriielfeliciano <gabrielfelicianogo@gmail.com>
2020-10-15 19:28:59 -0300

    Implementação Filtro

--- fornecedor.php
+++ fornecedor.php
@@ -3,24 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <title>Fornecedores</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <?php include "include/painel.php";?>
     
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
+
     <script>
         $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').trigger('focus')
         })
     </script>
+    <script>
+$(document).ready(function() {
+    $('#tabela').DataTable( {
+      paging:   false,
+      info:     false,
+      dom: 'Bfrtip',
+
+        "language": {
+          "searchPlaceholder": "Pesquisar ",
+          "sEmptyTable": "Nenhum registro encontrado",
+          "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+          "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+          "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+          "sInfoPostFix": "",
+          "sInfoThousands": ".",
+          "sLengthMenu": "_MENU_ resultados por página",
+          "sLoadingRecords": "Carregando...",
+          "sProcessing": "Processando...",
+          "sZeroRecords": "Nenhum registro encontrado",
+          "sSearch": "",
+        "oPaginate": {
+          "sNext": "Próximo",
+          "sPrevious": "Anterior",
+          "sFirst": "Primeiro",
+          "sLast": "Último"
+                    },
+        "oAria": {
+          "sSortAscending": ": Ordenar colunas de forma ascendente",
+          "sSortDescending": ": Ordenar colunas de forma descendente"
+                }
+                    }
+    });
+});
+
+</script>
    <?php include "include/menu.php";?>
    <div class="container">
    <h1 class="display-4 text-center">Fornecedores</h1>
    <a href="cad_fornecedor.php" class="btn btn-outline-success" style="margin-bottom:2%;">Adicionar Fornecedor</a>
    </div>
-        <table class="table table-striped container ">
+        <table class="table table-striped container " id="tabela">
             <thead>
               <tr>
                 <th scope="col">Fornecedor</th>
