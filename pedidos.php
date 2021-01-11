df227e1a84076da366f96c0eae91718cfe61ae67
LFirmiano <lucas.firmianosg@gmail.com>
2021-01-11 15:29:44 -0300

    PARTE1 TROCA

--- pedidos.php
+++ pedidos.php
@@ -64,18 +64,18 @@ $(document).ready(function() {
             <tbody>
               <tr>
                 <?php 
-                    include "include/L_pedido.php";
+                    include "include/L_todos_pedidos.php";
                       while($row = $stmt->fetch(PDO::FETCH_OBJ)){
                 ?>
-                <th scope="row">aa<?php //echo $row->qtd_pedidos_item ?></th>
-                <td>teste<?php //echo $row->valor_final ?></td>
-                <td>teste<?php //echo date_format(new DateTime($row->data),'H:i:s') ?></td>
+                <th scope="row"><?php echo $row->qtd_pedidos_item ?></th>
+                <td><?php echo $row->valor_final ?></td>
+                <td><?php echo date_format(new DateTime($row->data),'d/m/Y') ?></td>
                 <td>
 
                 <!--botao detalhes-->
                 <form action="detalhe_pedido.php" method="POST">
-                  <input type="hidden" value="<?php //echo $row->id_pedido ?>" name="visualizar">
-                  <input type="hidden" value="<?php //echo $row->data ?>" name="data">
+                  <input type="hidden" value="<?php echo $row->id_pedido ?>" name="visualizar">
+                  <input type="hidden" value="<?php echo $row->data ?>" name="data">
                   <button type="submit" class="btn btn-outline-dark" style="float: left; margin-right: 3%;"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                   </svg></button>
@@ -83,8 +83,8 @@ $(document).ready(function() {
 
                 <!--botão visualizar produtos-->
                 <form action="produtos_pedido.php" method="POST">
-                  <input type="hidden" value="<?php //echo $row->id_pedido ?>" name="visualizar">
-                  <input type="hidden" value="<?php //echo $row->data ?>" name="data">
+                  <input type="hidden" value="<?php echo $row->id_pedido ?>" name="visualizar">
+                  <input type="hidden" value="<?php echo $row->data ?>" name="data">
                   <button type="submit" class="btn btn-outline-dark" style="float: left; margin-right: 3%;"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z"/>
                   <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
--- trocar_produto.php
+++ trocar_produto.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Troca de Produto</title>
-    <?php include "include/painel2.php" ?>
+    <?php include "include/painel2.php";
+    include "include/R_pedido_item_troca.php"; ?>
 </head>
 <body>
     <script>
@@ -11,7 +12,9 @@
         $('#myInput').trigger('focus')
         })
     </script>
-   <?php include "include/menu.php";?>
+   <?php include "include/menu.php";
+   include "include/L_estoque.php";
+   ?>
    
    <form method="POST" action="">
    <div class="container">
@@ -20,19 +23,20 @@
    <fieldset disabled>
     <div class="form-group-inline">
       <label for="disabledTextInput"><strong>Produto:</strong></label>
-      <input type="text" id="prod" class="form-control"  value="Short Jeans<?php //echo $prod->produto ?>">
+      <input type="text" id="prod" class="form-control"  value="<?php echo $row->produto." ".$row->categoria ?>">
 
       <label for="disabledTextInput"><strong>Dia da compra:</strong></label>
-      <input type="text" id="disabledTextInput" class="form-control" value="02/02/2021<?php //echo $prod->categoria ?>">
+      <input type="text" id="disabledTextInput" class="form-control" value="<?php echo date_format(new DateTime($row->hora_compra),'d/m/Y') ?>">
 
-      <label for="disabledTextInput"><strong>Valor de Compra:</strong></label>
-      <input type="text" id="disabledTextInput" class="form-control" value="R$ 20.00<?php //echo $prod->valor_compra ?> ">
+      <label for="disabledTextInput"><strong>Valor de venda:</strong></label>
+      <input type="text" id="disabledTextInput" class="form-control" value="R$ <?php echo $row->valor ?> ">
+      <input type="hidden" id="pegartot" value="<?php echo $row->valor ?>">
 
       <label for="disabledTextInput"><strong>Tamanho:</strong></label>
-      <input type="text" id="disabledTextInput" class="form-control" value="M <?php //echo $prod->valor_compra ?> ">
+      <input type="text" id="disabledTextInput" class="form-control" value="<?php echo $row->tamanho ?> ">
 
       <label for="disabledTextInput"><strong>Quantidade:</strong></label>
-      <input type="text" id="disabledTextInput" class="form-control" value="2<?php //echo $prod->valor_compra ?> ">
+      <input type="text" id="disabledTextInput" class="form-control" value="<?php echo $row->quantidade ?> ">
     </div>
 
   </fieldset>
@@ -40,23 +44,43 @@
    <!--form troca de produto-->
 
    <div class="row" style="margin-top:2%; margin-bottom:5%">
+  
+   <div class="form-group col-md-3">
+  <label for="exampleFormControlSelect1"><strong>Quantidade para troca</strong></label>
+  
+  <select class="form-control" id="" name="qtdantiga" onchange="calculartot(this)">
+      <option value="">Selecione a Quantidade</option>
+      <?php 
+        for ($l = 1; $l <= $row->quantidade; $l++){
+      ?>
+        <option value="<?=$l?>"><?=$l?></option>
+      <?php } ?>
+  </select>
 
+  </div>
+
+  <div class="form-group col-md-3">
+    <label for="exampleFormControlSelect1"><strong>Valor</strong></label>
+    <input type="text" class="form-control" id="valtot" name="" disabled>
+  </div>
+
+  <div class="form-group col-md-6"></div>
 
    <div class="form-group col-md-8">
     <label for="exampleFormControlSelect1"><strong>Novo produto</strong></label>
     <select class="form-control" onchange="getval(this)" name="id" id="id">
     <option value="">Selecione o Produto</option>
     <?php 
-      //$i=0; 
-      /*while ($row = $stmt->fetch(PDO::FETCH_OBJ)){ 
+      $i=0; 
+      while ($row = $stmt->fetch(PDO::FETCH_OBJ)){ 
         $data[] = array(
           'produto' => $row->produto,
           'valor' => $row->valor,
           'id' => $row->id_produto
-        );*/
+        );
       ?>
-      <option value="<?php //echo $row->id_produto ?>">teste<?php //echo $row->produto." ".$row->categoria." (R$".$row->valor.")" ?></option>
-    <?php //} ?>
+      <option value="<?php echo $row->id_produto ?>"><?php echo $row->produto." ".$row->categoria." (R$".$row->valor.")" ?></option>
+    <?php } ?>
     </select>
   </div>
   <div class="form-group col-md-4">
@@ -96,3 +120,26 @@
   </div>
 </body>
 </html>
+
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script>
+function getval(sel)
+    {
+        // window.alert(sel.value);
+        $.getJSON('include/R_estoque_ajax.php?search=',{id: sel.value, ajax: 'true'}, function(j){
+          var opt = '<option>Selecione o Tamanho</option>';	
+          for (var i = 0; i < j.length; i++) {
+            opt += '<option value="' + j[i].tamanhos + '">' + j[i].tamanhos + '</option>';
+          }	
+          $('#unidades').html(opt).show();
+  });
+    }
+
+function calculartot(sel){
+  qtd = $(sel).val()
+  valor = $('#pegartot').val()
+  result = qtd*valor
+  $('#valtot').val('R$ '+result+'.00')
+}
+</script>
