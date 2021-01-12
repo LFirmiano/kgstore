550408a67294d5e0ee9df9d81d0434cd66666af5
LFirmiano <lucas.firmianosg@gmail.com>
2021-01-12 14:38:47 -0300

    ORDENAMENTOS

--- include/L_caixa.php
+++ include/L_caixa.php
@@ -2,7 +2,7 @@
 
 include_once("bd.php");
 
-$stmt2 = $conn->prepare("SELECT * FROM caixa WHERE id_caixa = :id");
+$stmt2 = $conn->prepare("SELECT * FROM caixa WHERE id_caixa = :id ORDER BY data_caixa DESC");
 $id = 1;
 $stmt2->bindParam(':id',$id,PDO::PARAM_STR);
 if ($stmt2->execute()){
