06d0f6ee9581da9a9788ad6da28748968225799a
LFirmiano <lucas.firmianosg@gmail.com>
2021-02-03 16:34:22 -0300

    CORRECAO BUG VIEW PEDIDO ITEM

--- include/R_pedido_item.php
+++ include/R_pedido_item.php
@@ -2,14 +2,13 @@
 
 include "bd.php";
 
-if (!isset($_SESSION['data'])){
-    $_SESSION['data'] = $_POST['data'];
-}else{
-    $_SESSION['data'] = $_POST['data'];
-}
+$_SESSION['data'] = $_POST['data'];
+
+// var_dump($_SESSION['data']);
 
 $query = "SELECT * FROM pedido_item JOIN produtos p ON pedido_item.produto_id = p.id_produto  WHERE hora_compra = :hora_compra";
 $stmt = $conn->prepare($query);
 $stmt->bindParam(':hora_compra',$_SESSION['data'],PDO::PARAM_STR);
 $stmt->execute();
+
 ?>
